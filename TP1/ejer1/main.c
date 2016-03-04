a5e56838118ee38fe0de0f7d5f5a472ca9d682bc
francotesei <ftesei96@gmail.com>
2016-03-03 22:23:24 -0300

    added ejer1

--- TP1/ejer1/main.c
+++ TP1/ejer1/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+long int factorial(const int n){
+
+    return (n==0)?1:n*factorial(n-1);
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("%d",factorial(1));
+    printf("\n");
     return 0;
 }
